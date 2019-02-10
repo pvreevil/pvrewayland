c13e4dcc43593d0e409de6b52266c3375edbfafd
pvreevil <zabieldblack@cumallover.me>
2019-02-10 17:54:43 -0300

    bar

--- home/.config/sway/conf/abar/abar1v2.sh
+++ home/.config/sway/conf/abar/abar1v2.sh
@@ -4,7 +4,6 @@ pkill requestsb-
 
 export BARPATH=~/.config/sway/conf/
 
-KERNEL=$(uname -r)
 $BARPATH/netspeed/requestsb-netspeed.sh &
 $BARPATH/github/requestsb-github.sh  &
 $BARPATH/newsboat/requestsb-newsboat.sh      &
@@ -20,9 +19,13 @@ b="\"background\":"
 s="\"separator\": \"false\""
 sw="\"separator_block_width\":"
 mw="\"min_width\":"
-ac="\"align\":\"center\""
+ac="\"align\": \"center\""
 ft="\"full_text\":"
 bor="\"border\": \"$colorf\""
+mu="\"markup\": \"pango\""
+rarrow="{$c \"$color4\",$b \"$color1\",$s,$sw \"0\",$ac,$mu,$ft \"<span font_desc='GoMono Nerd Font Mono 22'></span>\"},"
+rarrow2="{$c \"$color4\",$b \"$00000000\",$s,$sw \"0\",$ac,$mu,$ft \"<span font_desc='GoMono Nerd Font Mono 22'></span>\"},"
+larrow="{$c \"$color4\",$b \"$00000000\",$s,$sw \"0\",$ac,$mu,$ft \"<span font_desc='GoMono Nerd Font Mono 22'></span>\"},"
 echo '{ "version" : 1 }'
 echo '['
 echo '[]'
@@ -42,32 +45,28 @@ while
 	getvol
 	printf %s	",[
 
-	{$c \"$gc\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 40,$ft \"\"},
-	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 55,$ft \"$GPU%\"},
-        {$c \"$mc\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 40,$ft \"\"},
-        {$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 55,$ft \"$MEM%\"},
-	{$c \"$cc\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 40,$ft \"\"},
-	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 55,$ft \"$CPU%\"},
-	{$c \"$colorf\",$b \"$color1\",$s,$sw\"0\",$ac,$mw 40,$ft \"\"},
-	{$c \"$colorf\",$b \"$color4\",$s,$sw\"0\",$ac,$mw 55,$ft \"$(cat $BARPATH/netspeed/speed | awk '{print $1 $2}')\"},
-	{$c \"$colorf\",$b \"$color1\",$s,$sw\"0\",$ac,$mw 40,$ft \"\"},
-	{$c \"$colorf\",$b \"$color4\",$s,$sw\"0\",$ac,$mw 55,$ft \"$(cat $BARPATH/netspeed/speed | awk '{print $3 $4}')\"},
-	{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 40,$ft \"\"},
-	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 55,$ft \"$GH\"},
-	{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 40,$ft \"\"},
-	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 55,$ft \"$RDD\"},
-	{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 40,$ft \"\"},
-	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 55,$ft \"$RSS\"},
-	{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 40,$ft \"$VOLI\"},
-	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 55,$ft \"$VOL\"},
-	{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 40,$ft \"\"},
-	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 170,$ft \"$(date +'%A, %d')\"},
-	{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 40,$ft \"\"},
-	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 80,$ft \"$(date +'%H:%M')\"},
-	{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 40,$ft \"\"},
-	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 80,$ft \"$(uptime --pretty | sed 's/up //' | sed 's/\ years\?,/y/' | sed 's/\ days\?,/d/' | sed 's/\ hours\?,\?/h/' | sed 's/\ minutes\?/m/')\"},
-	{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 40,$ft \"\"},
-	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 170,$ft \"$KERNEL\"},
+	$larrow {$c \"$gc\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},$rarrow
+	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 40,$ft \"$GPU%\"},$rarrow2
+        {$c \"$mc\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},$rarrow
+        {$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 40,$ft \"$MEM%\"},$rarrow2
+	{$c \"$cc\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},$rarrow
+	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 40,$ft \"$CPU%\"},$rarrow2
+	{$c \"$colorf\",$b \"$color1\",$s,$sw\"0\",$ac,$mw 30,$ft \"\"},$rarrow
+	{$c \"$colorf\",$b \"$color4\",$s,$sw\"0\",$ac,$mw 55,$ft \"$(cat $BARPATH/netspeed/speed | awk '{print $1 $2}')\"},$rarrow2
+	{$c \"$colorf\",$b \"$color1\",$s,$sw\"0\",$ac,$mw 30,$ft \"\"},$rarrow
+	{$c \"$colorf\",$b \"$color4\",$s,$sw\"0\",$ac,$mw 55,$ft \"$(cat $BARPATH/netspeed/speed | awk '{print $3 $4}')\"},$rarrow2
+	{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},$rarrow
+	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 30,$ft \"$GH\"},$rarrow2
+	{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},$rarrow
+	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 30,$ft \"$RDD\"},$rarrow2
+	{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},$rarrow
+	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 30,$ft \"$RSS\"},$rarrow2
+	{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"$VOLI\"},$rarrow
+	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 30,$ft \"$VOL\"},$rarrow2
+	{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},$rarrow
+	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 120,$ft \"$(date +'%A, %d') \"},$rarrow2
+	{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},$rarrow
+	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 40,$ft \" $(date +'%H:%M')\"},$rarrow2
 		]"
 do sleep 0.2
 done
