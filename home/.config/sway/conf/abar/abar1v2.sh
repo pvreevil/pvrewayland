a313a2861ab090282951db9e9e85c14ca6b81b6f
pvreevil <zabieldblack@cumallover.me>
2019-02-10 21:59:54 -0300

    fixes

--- home/.config/sway/conf/abar/abar1v2.sh
+++ home/.config/sway/conf/abar/abar1v2.sh
@@ -23,9 +23,9 @@ ac="\"align\": \"center\""
 ft="\"full_text\":"
 bor="\"border\": \"$colorf\""
 mu="\"markup\": \"pango\""
-rarrow="{$c \"$color4\",$b \"$color1\",$s,$sw \"-2\",$ac,$mu,$ft \"<span font_desc='GoMono Nerd Font Mono 22'></span>\"},"
-rarrow2="{$c \"$color4\",$b \"$color1\",$s,$sw \"-2\",$ac,$mu,$ft \"<span font_desc='GoMono Nerd Font Mono 22'></span>\"},"
-larrow="{$c \"$color4\",$b \"$color1\",$s,$sw \"-2\",$ac,$mu,$ft \"<span font_desc='GoMono Nerd Font Mono 22'></span>\"},"
+larrow="{$c \"$color4\",$b \"$color1\",$s,$sw \"0\",$ac,$mu,$ft \"<span font_desc='GoMono Nerd Font Mono 25'></span>\"},"
+rarrow="{$c \"$color4\",$b \"$color1\",$s,$sw \"0\",$ac,$mu,$ft \"<span font_desc='GoMono Nerd Font Mono 25'></span>\"},"
+larrow2="{$c \"$color4\",$b \"$color1\",$s,$sw \"0\",$ac,$mu,$ft \"<span font_desc='GoMono Nerd Font Mono 25'></span>\"},"
 echo '{ "version" : 1 }'
 echo '['
 echo '[]'
@@ -45,29 +45,29 @@ while
 	getvol
 	printf %s	",[
 
-	$larrow {$c \"$gc\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},$rarrow
-	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 40,$ft \"$GPU%\"},$rarrow2
-        {$c \"$mc\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},$rarrow
-        {$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 40,$ft \"$MEM%\"},$rarrow2
-	{$c \"$cc\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},$rarrow
-	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 40,$ft \"$CPU%\"},$rarrow2
-	{$c \"$colorf\",$b \"$color1\",$s,$sw\"0\",$ac,$mw 30,$ft \"\"},$rarrow
-	{$c \"$colorf\",$b \"$color4\",$s,$sw\"0\",$ac,$mw 55,$ft \"$(cat $BARPATH/netspeed/speed | awk '{print $1 $2}')\"},$rarrow2
-	{$c \"$colorf\",$b \"$color1\",$s,$sw\"0\",$ac,$mw 30,$ft \"\"},$rarrow
-	{$c \"$colorf\",$b \"$color4\",$s,$sw\"0\",$ac,$mw 55,$ft \"$(cat $BARPATH/netspeed/speed | awk '{print $3 $4}')\"},$rarrow2
-	{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},$rarrow
-	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 30,$ft \"$GH\"},$rarrow2
-	{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},$rarrow
-	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 30,$ft \"$RDD\"},$rarrow2
-	{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},$rarrow
-	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 30,$ft \"$RSS\"},$rarrow2
-	{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"$VOLI\"},$rarrow
-	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 30,$ft \"$VOL\"},$rarrow2
-	{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},$rarrow
-	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 120,$ft \"$(date +'%A, %d')\"},$rarrow2
-	{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},$rarrow
-	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 40,$ft \"$(date +'%H:%M')\"},
-	{$c \"$color4\",$b \"$00000000\",$s,$sw \"-2\",$ac,$mu,$ft \"<span font_desc='GoMono Nerd Font Mono 22'></span>\"},
+	$larrow2 {$c \"$gc\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},
+	$larrow {$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 40,$ft \"$GPU%\"},$rarrow
+        	{$c \"$mc\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},
+  	$larrow {$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 40,$ft \"$MEM%\"},$rarrow
+		{$c \"$cc\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},
+	$larrow {$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 40,$ft \"$CPU%\"},$rarrow
+		{$c \"$colorf\",$b \"$color1\",$s,$sw\"0\",$ac,$mw 30,$ft \"\"},
+	$larrow {$c \"$colorf\",$b \"$color4\",$s,$sw\"0\",$ac,$mw 55,$ft \"$(cat $BARPATH/netspeed/speed | awk '{print $1 $2}')\"},$rarrow
+		{$c \"$colorf\",$b \"$color1\",$s,$sw\"0\",$ac,$mw 30,$ft \"\"},
+	$larrow {$c \"$colorf\",$b \"$color4\",$s,$sw\"0\",$ac,$mw 55,$ft \"$(cat $BARPATH/netspeed/speed | awk '{print $3 $4}')\"},$rarrow
+		{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},
+	$larrow	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 30,$ft \"$GH\"},$rarrow
+		{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},
+	$larrow {$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 30,$ft \"$RDD\"},$rarrow
+		{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},
+	$larrow	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 30,$ft \"$RSS\"},$rarrow
+		{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"$VOLI\"},
+	$larrow	{$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 30,$ft \"$VOL\"},$rarrow
+		{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},
+	$larrow {$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 120,$ft \"$(date +'%A, %d')\"},$rarrow
+		{$c \"$colorf\",$b \"$color1\",$s,$sw \"0\",$ac,$mw 30,$ft \"\"},
+	$larrow {$c \"$colorf\",$b \"$color4\",$s,$sw \"0\",$ac,$mw 40,$ft \"$(date +'%H:%M')\"},
+		{$c \"$color4\",$b \"#ff000000\",$s,$sw \"0\",$ac,$mu,$ft \"<span font_desc='GoMono Nerd Font Mono 22'></span>\"},
 		]"
 do sleep 0.2
 done
--- home/.config/sway/conf/abar/colorsnaturalist.sh
+++ home/.config/sway/conf/abar/colorsnaturalist.sh
@@ -7,10 +7,10 @@ colorl=#3a4a3f
 colorm=#acaa6c
 colorh=#c84f68
 
-color1=#cdd2d2f2
-color2=#c8bcabf2
-color3=#979083f2
-color4=#d4b8b3f2
+color1=#cdd2d2
+color2=#c8bcab
+color3=#979083
+color4=#d4b8b3
 
 
 
