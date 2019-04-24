18a815043a7cd8f9f547c645fc696de71ef9ca3e
pvreevil <zabieldblack@cumallover.me>
2019-04-23 23:27:39 -0300

    mocp cover on notifications

--- home/.local/bin/mocnot.sh
+++ home/.local/bin/mocnot.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-notify-send.sh -t 9000 "$(mocp -Q %a) - $(mocp -Q %song)"
+
+p=`dirname "$1"`
+if [[ -f $p/folder.png ]]; then
+	cp "$p/folder.png" /tmp/folder.png
+	notify-send.sh -i /tmp/folder.png -t 9000 "$(mocp -Q %a) - $(mocp -Q %song)"
+elif [[ -f $p/../folder.png ]]; then
+	cp "$p/../folder.png" /tmp/folder.png
+	notify-send.sh -i /tmp/folder.png -t 9000 "$(mocp -Q %a) - $(mocp -Q %song)"
+else
+	notify-send.sh -t 9000 "$(mocp -Q %a) - $(mocp -Q %song)"
+fi
