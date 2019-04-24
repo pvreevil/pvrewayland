0e3caa9ea833bcb38b54c498c339a03f3038a8bb
pvreevil <zabieldblack@cumallover.me>
2019-04-24 18:56:05 -0300

    mocn

--- home/.local/bin/mocnot.sh
+++ home/.local/bin/mocnot.sh
@@ -6,13 +6,13 @@ imregex="(Folder|folder|front|Front|Cover|cover|art|Art)\\.(jpg|jpeg|png|gif)$"
 cover=`ls "$p" | grep -E -i -m1 $imregex`
 coverup=`ls "$p/.." | grep -E -i -m1 $imregex`
 
-if [[ -f $p/$cover ]]; then
+if [ -f "$p"/"$cover" ]; then
 
 	cp "$p/$cover" /tmp/cover.png
 
 	notify-send.sh -i /tmp/cover.png -t 9000 "$(mocp -Q "%a - %song")"
 
-elif [[ -f $p/../$coverup ]]; then
+elif [ -f "$p"/../"$coverup" ]; then
 
 	cp "$p/../$coverup" /tmp/cover.png
 
